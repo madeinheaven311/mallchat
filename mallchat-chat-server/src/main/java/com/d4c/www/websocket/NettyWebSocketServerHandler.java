8f5fb330154b7cfa0367ced8b9b4d0fe4fc17203
D4C <545158835@qq.com>
2025-06-02 22:07:35 +0800

    add: Netty模块编写

---
+++ mallchat-chat-server/src/main/java/com/d4c/www/websocket/NettyWebSocketServerHandler.java
@@ -0,0 +1,20 @@
+package com.d4c.www.websocket;
+
+
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@Sharable
+public class NettyWebSocketServerHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, TextWebSocketFrame textWebSocketFrame) throws Exception {
+
+    }
+}
--- mallchat-framework/src/main/java/com/d4c/www/Main.java
+++
@@ -1,7 +0,0 @@
-package com.d4c.www;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
